19cf77414a8b4a36274adbd33c39d694d053a844
Piotr Święs <10480513+pswies@users.noreply.github.com>
2025-09-01 16:15:14 +0200

    chore: upload traces to S3

--- src/utils/helpers/tracing.ts
+++ src/utils/helpers/tracing.ts
@@ -1,14 +1,35 @@
 import path from "path";
 import fs from "fs";
 import { logger } from "../logger/logging-utils";
-import extract from 'extract-zip';
+import JSZip from "jszip";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 
-export async function processTraceFile(tracePath: string) {
+export async function processTraceFile(tracePath: string, routeId: string, timestamp: string) {
   logger.info("Processing trace file", { tracePath });
 
+  // Unzip the trace file
   const data = fs.readFileSync(tracePath);
-  await extract(tracePath, { dir: path.dirname(tracePath) });
+  const zip = await JSZip.loadAsync(data);
+  const files = Object.keys(zip.files);
+
+  // Upload the trace file to S3
+  const s3 = new S3Client({
+    region: process.env.AWS_REGION,
+  });
+
+  // Upload each trace file to S3 under a common directory
+  for (const file of files) {
+    if (!zip.files[file].dir) { // Skip directories - we assume the only file is the trace file
+      const fileContent = await zip.files[file].async("nodebuffer");
+      const uploadParams = {
+        Bucket: process.env.AWS_TRACES_BUCKET_NAME,
+        Key: `${routeId}/${timestamp}/${file}`,
+        Body: fileContent,
+      };
+      await s3.send(new PutObjectCommand(uploadParams));
+    }
+  }
 
   // Remove the zip file after extraction
   fs.unlinkSync(tracePath);
