c30bb8df2de3ac32ab0e4812b19f637c3d48e841
Piotr Święs <10480513+pswies@users.noreply.github.com>
2025-09-02 12:34:18 +0200

    chore: simplify resources names in ECS

--- terraform/schedule.tf
+++ terraform/schedule.tf
@@ -24,7 +24,7 @@ resource "aws_cloudwatch_log_group" "this" {
 resource "aws_ecs_task_definition" "this" {
   for_each = { for test in local.test_definitions : test.id => test }
 
-  family                   = "test-${each.value.id}"
+  family                   = each.value.id
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = "256"
@@ -76,7 +76,7 @@ resource "aws_cloudwatch_event_target" "run_tasks" {
   for_each = { for test in local.test_definitions : test.id => test }
 
   rule      = aws_cloudwatch_event_rule.test_schedules[each.key].name
-  target_id = "test-${each.value.id}-fargate-task"
+  target_id = each.value.id
   arn       = aws_ecs_cluster.this.arn
   role_arn  = aws_iam_role.events_invoke_ecs.arn
 
