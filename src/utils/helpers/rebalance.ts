8b3735f103fe62cd31eb6b1ef6388d75071785f4
Dimitar Petkov <796902+2dpetkov@users.noreply.github.com>
2025-09-16 16:28:48 +0300

    nit: make rebalance more abstact and beautiful

--- src/utils/helpers/rebalance.ts
+++ src/utils/helpers/rebalance.ts
@@ -12,93 +12,97 @@ export { rebalanceNow };
 async function rebalanceNow(route: Route): Promise<{ balancesBefore: BalanceMap; balancesAfter: BalanceMap }> {
   interop.configureSkipClient();
 
-  let balancesBefore: BalanceMap = [];
-  let balancesAfter: BalanceMap = [];
-
   switch (route.kind) {
     case 'deposit':
-      balancesBefore = [
-        // on source chain:
-        {
-          token: 'USDC',
-          chain: route.src_chain,
-          amount: (await interop.getUsdcBalance(CHAIN_IDS[route.src_chain], route.wallet_address)).formattedAmount,
-        },
-        // on dYdX:
-        {
-          token: 'USDC',
-          chain: route.dst_chain,
-          amount: (await interop.getFreeCollateral(route.dydx_address)).formattedAmount,
-        },
-      ];
-
-      await interop.withdrawMaxUsdc(
-        route.dydx_address,
-        route.dydx_seed,
-        route.src_chain,
-        route.wallet_address,
-        route.wallet_seed
-      );
-
-      balancesAfter = [
-        // on source chain:
-        {
-          token: 'USDC',
-          chain: route.src_chain,
-          amount: (await interop.getUsdcBalance(CHAIN_IDS[route.src_chain], route.wallet_address)).formattedAmount,
-        },
-        // on dYdX:
-        {
-          token: 'USDC',
-          chain: route.dst_chain,
-          amount: (await interop.getFreeCollateral(route.dydx_address)).formattedAmount,
-        },
-      ];
-
-      break;
+      return await rebalanceDepositRoute(route);
     case 'withdraw':
-      balancesBefore = [
-        // on dYdX:
-        {
-          token: 'USDC',
-          chain: route.src_chain,
-          amount: (await interop.getFreeCollateral(route.dydx_address)).formattedAmount,
-        },
-        // on destination chain:
-        {
-          token: 'USDC',
-          chain: route.dst_chain,
-          amount: (await interop.getUsdcBalance(CHAIN_IDS[route.dst_chain], route.wallet_address)).formattedAmount,
-        },
-      ];
-
-      await interop.depositMaxUsdc(
-        route.dst_chain,
-        route.wallet_address,
-        route.wallet_seed,
-        route.dydx_address,
-        route.dydx_seed
-      );
-
-      balancesAfter = [
-        // on dYdX:
-        {
-          token: 'USDC',
-          chain: route.src_chain,
-          amount: (await interop.getFreeCollateral(route.dydx_address)).formattedAmount,
-        },
-        // on destination chain:
-        {
-          token: 'USDC',
-          chain: route.dst_chain,
-          amount: (await interop.getUsdcBalance(CHAIN_IDS[route.dst_chain], route.wallet_address)).formattedAmount,
-        },
-      ];
-
-      break;
+      return await rebalanceWithdrawRoute(route);
     default:
       throw new Error(`Invalid route kind: ${route.kind}`);
   }
+}
 
+async function rebalanceDepositRoute(route: Route): Promise<{ balancesBefore: BalanceMap; balancesAfter: BalanceMap }> {
+  interop.configureSkipClient();
+  const balancesBefore = [
+    // on source chain:
+    {
+      token: 'USDC',
+      chain: route.src_chain,
+      amount: (await interop.getUsdcBalance(CHAIN_IDS[route.src_chain], route.wallet_address)).formattedAmount,
+    },
+    // on dYdX:
+    {
+      token: 'USDC',
+      chain: route.dst_chain,
+      amount: (await interop.getFreeCollateral(route.dydx_address)).formattedAmount,
+    },
+  ];
+
+  await interop.withdrawMaxUsdc(
+    route.dydx_address,
+    route.dydx_seed,
+    route.src_chain,
+    route.wallet_address,
+    route.wallet_seed
+  );
+
+  const balancesAfter = [
+    // on source chain:
+    {
+      token: 'USDC',
+      chain: route.src_chain,
+      amount: (await interop.getUsdcBalance(CHAIN_IDS[route.src_chain], route.wallet_address)).formattedAmount,
+    },
+    // on dYdX:
+    {
+      token: 'USDC',
+      chain: route.dst_chain,
+      amount: (await interop.getFreeCollateral(route.dydx_address)).formattedAmount,
+    },
+  ];
+  return { balancesBefore, balancesAfter };
+}
+
+async function rebalanceWithdrawRoute(
+  route: Route
+): Promise<{ balancesBefore: BalanceMap; balancesAfter: BalanceMap }> {
+  const balancesBefore = [
+    // on dYdX:
+    {
+      token: 'USDC',
+      chain: route.src_chain,
+      amount: (await interop.getFreeCollateral(route.dydx_address)).formattedAmount,
+    },
+    // on destination chain:
+    {
+      token: 'USDC',
+      chain: route.dst_chain,
+      amount: (await interop.getUsdcBalance(CHAIN_IDS[route.dst_chain], route.wallet_address)).formattedAmount,
+    },
+  ];
+
+  await interop.depositMaxUsdc(
+    route.dst_chain,
+    route.wallet_address,
+    route.wallet_seed,
+    route.dydx_address,
+    route.dydx_seed
+  );
+
+  const balancesAfter = [
+    // on dYdX:
+    {
+      token: 'USDC',
+      chain: route.src_chain,
+      amount: (await interop.getFreeCollateral(route.dydx_address)).formattedAmount,
+    },
+    // on destination chain:
+    {
+      token: 'USDC',
+      chain: route.dst_chain,
+      amount: (await interop.getUsdcBalance(CHAIN_IDS[route.dst_chain], route.wallet_address)).formattedAmount,
+    },
+  ];
   return { balancesBefore, balancesAfter };
 }
